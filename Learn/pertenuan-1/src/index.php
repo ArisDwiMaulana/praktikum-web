a86d95b6c0c53044388203f7c8fa77aa6e2290f3
ArisDwiMaulana <dwiaris3765@gmail.com>
2026-09-02 20:16:37 +0700

    membuat halaman web dengan layout sederhana

---
+++ Learn/pertenuan-1/src/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="container-judul">
+            <h1 class="judul">Buat Website</h1>
+        </div>
+        <div class="container-menu">
+            <a href="" class="menu">Home</a>
+            <a href="" class="menu">Profil</a>
+            <a href="" class="menu">Produk</a>
+            <a href="" class="menu">Kontak</a>
+        </div>
+        <div class="content">
+            <h1 class="judul-artikel">Linux</h1>
+            <div class="container-image">
+                <img src="https://imgs.search.brave.com/5U9ej4DVgoqA-DOYONUenwXQJ4EizvCmMl24sf3FxXw/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAyMi8w/My8yMC8wMy8xNy93/aW5kb3dzLTcwNzk4/NzZfNjQwLnBuZw"
+                    alt="" id="gambar-artikel" width="100%" height="100%">
+            </div>
+            <div class="container-paragraf">
+                <p class="paragraf-artikel">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum asperiores
+                    provident laboriosam voluptate! Magni ut perspiciatis minus nesciunt porro perferendis ducimus,
+                    deserunt qui consequatur maiores earum facilis debitis recusandae temporibus?</p>
+                <p class="paragraf-artikel">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus
+                    excepturi nihil minima nemo saepe iure perspiciatis pariatur error voluptas hic totam at, quidem est
+                    repellat autem omnis qui sit odio.</p>
+            </div>
+        </div>
+        <div class="footer">
+            <h3 class="text-footer">
+                Copyright (c) 2026 Author. All Rights Reserved.
+            </h3>
+        </div>
+</body>
+
+</html>
