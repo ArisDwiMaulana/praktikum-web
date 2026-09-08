e8936237590fa00c43b950549a25f833421edb65
ArisDwiMaulana <dwiaris3765@gmail.com>
2026-09-08 14:25:04 +0700

    pertemuan 2

---
+++ Learn/pertemuan-2/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="./style.css">
+</head>
+<body>
+	<div class="container">
+	    <h1 class="judul">Website Technology</h1>
+		<div class="container-link">
+		    <a href="" class="link">Home</a>
+			<a href="" class="link">Profil</a>
+			<a href="" class="link">Produk</a>
+			<a href="" class="link">Kontak</a>
+		</div>
+		<div class="container-content">
+		    <h2 class="judul-artikel">Apa Itu Internet</h2>
+			<div class="content">
+			    <div class="image">
+					<img src="https://imgs.search.brave.com/mSikdbxuIcTlqBJ2s97Kur6PRin5xAyzR0NtDKO1qeI/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMDcv/NDUyLzMyOS9zbWFs/bC9tYW4tdW5sb2Nr/cy1maWxlcy1pbi10/aGUtY2xvdWQtaW50/ZXJuZXQtdGVjaG5v/bG9neS1jb25jZXB0/LW9mLWdsb2JhbC1k/YXRhLWNvbm5lY3Rp/b24tYnktbWV0YXZl/cnNlLXRlY2hub2xv/Z3ktYmlnLWRhdGEt/c2VhcmNoLWZvci1p/bmZvcm1hdGlvbi1v/bmxpbmUtYnVzaW5l/c3MtZ2xvYmFsLW5l/dHdvcmstaW50ZXJu/ZXQtb2YtdGhpbmdz/LWZyZWUtcGhvdG8u/anBn" alt="" width="100%">
+				</div>
+				<div class="paragraf">
+				    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi architecto fugit nihil accusantium quas dolore eaque dolorem, ratione autem voluptas repellendus explicabo itaque non iusto, est quidem? Facilis, quos eaque.</p>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab aliquam quibusdam impedit corrupti. Dignissimos voluptatum asperiores voluptatibus corrupti. Quaerat obcaecati laboriosam voluptatibus exercitationem quas numquam voluptates ipsam error eos quasi!</p>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab aliquam quibusdam impedit corrupti. Dignissimos voluptatum asperiores voluptatibus corrupti. Quaerat obcaecati laboriosam voluptatibus exercitationem quas numquam voluptates ipsam error eos quasi!</p>
+				</div>
+			</div>
+		</div>
+		<div class="footer-container">
+		    <h3 class="footer">Copyright @ 2026</h3>
+		</div>
+	</div>
+</body>
+</html>
